c638e0ae676ba2ffaffdf35fc8794d5f0e25ef87
Senzawa <senzawa.g@gmail.com>
2023-02-05 13:16:42 +0700

    String to Integer Convertor for alternate of stoi()

--- utils/stoi.cpp
+++ utils/stoi.cpp
@@ -5,8 +5,8 @@ int pow (int inp, int pow) {
   // 'inp' stands for Input, 'pow' stands for power
   int out = 1; // 'out' stands for output. Output will be 1 to make sure it's start counting from input^1
 
-  for (int i = 0; i < pow; i++) out *= inp; // Loop input calc until 'i' == 'pow'
-  if (pow == 0) inp = 1; // If any number powered with 0, it will return 1
+  if (pow == 0) out = 1; // If any number powered with 0, it will return 1
+  else for (int i = 0; i < pow; i++) out *= inp; // Loop input calc until 'i' == 'pow'
 
   return out;
 }
